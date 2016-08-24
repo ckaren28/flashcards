598e153162d55268f7a90d154cedcf1ec89b1baf
Sean Kendrick <seankendrick12@gmail.com>
2016-08-24 15:28:33 -0400

    finished subject/notecard controller

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 var User = require('./../controllers/users.js');
 var Collection = require('./../controllers/collections.js')
-var Subject = require('./../controllers/subject.js')
+var Subject = require('./../controllers/subject.js');
+var Notecard = require('./../controllers/notecard.js');
 
 
-module.exports = (function(app){
+module.exports = function(app){
     app.post('/register', User.register);
     app.post('/login', User.login);
     app.get('/getuser', User.getuser)
@@ -14,7 +15,15 @@ module.exports = (function(app){
     app.get('/addtotopcollections', Collection.addtotopcollections)
     app.post('/addcollection', Collection.addcollection)
     app.post('/editcollection/:id', Collection.editdescription)
-    app.get('/removecollection', Collection.removecollection)
+    app.get('/removecollection/:id', Collection.removecollection)
     app.get('/removecollectionfromtop', Collection.removecollectionfromtop)
-
-});
+    app.post('/addsubject', Subject.addsubject)
+    app.get('/getsubjects', Subject.getsubjects)
+    app.get('/getsubject/:id', Subject.getsubject)
+    app.post('/editsubject/:id', Subject.editsubject)
+    app.get('/removesubject/:id', Subject.removesubject)
+    app.post('/pushcard', Notecard.pushcard)
+    app.post('/pushcardatindex',Notecard.pushcardatindex)
+    app.post('/editcard/:id', Notecard.editcard)
+    app.get('/removecard')
+};
