9146216900b2cfbc2d3427a3fe909553b8facf80
Sean Kendrick <seankendrick12@gmail.com>
2016-08-25 08:49:46 -0400

    added controller methods

--- client/assets/controllers/subjectController.js
+++ client/assets/controllers/subjectController.js
@@ -1,22 +1,26 @@
-app.controller('profileController', [$scope, $location, profileFactory, $routeParams, function($scope, $location, profileFactory, $routeParams){
+app.controller('subjectController', ['$scope', '$location', 'subjectFactory', '$routeParams', '$cookies' function($scope, $location, subjectFactory, $routeParams, $cookies){
 
-//this is where the data for top collections will be stored.
-$scope.topCollections = {};
-//show the netflix scroll... so this should show all top collections from Database.
-$scope.index = function(){
-  userFactory.index(function(returned_Data){
-    $scope.topCollections = returned_Data.data
+$scope.subject = {}
+$scope.user = {}
+
+if($cookies.getObject('user')){
+	$scope.user = $cookies.getObject('user')
+}
+else{
+	$location.url('/')
+}
+
+$scope.get_subject = function(){
+  subjectFactory.show_subject($routeParams.id, function(data){
+  	$scope.subject = data
   })
 }
-index()
-
-//On a subject click (needs id)
-$scope.show_subject = function(){
-  userFactory.show_subject($routeParams.id, function(){
-    /// this goes to the subject page
-    //$location.url('/subjectpage')
-  })
+$scope.remove_subject = function(){
+	subjectFactory.remove_subject($routeParams.id, function(){
+		location.url('/profile')
+	})
 }
+
 
 
 }]);
