28154f18aaf2e0283a06542266ee0bfa470fdc3b
Karen Chin <c.karen28@gmail.com>
2016-08-24 21:34:59 -0400

    revised pages, added CSS

--- client/assets/app.js
+++ client/assets/app.js
@@ -5,9 +5,45 @@ app.config(function($routeProvider){ $routeProvider
         templateUrl: "partials/login.html",
         controller: "loginController"
     })
-    .when('/home', {
-        templateUrl: 'partials/wall.html',
-        controller: 'wallController'
+    .when('/profile', {
+        templateUrl: 'partials/profile.html',
+        controller: 'profileController'
+    })
+    .when('/collection/:id', {
+        templateUrl:'partials/one_collection.html',
+        controller: 'collectionController'
+    })
+    .when('/subject/:id ', {
+        templateUrl:'partials/subjects.html',
+        controller: 'subjectsController'
+    })
+    .when('/find_collection' , {
+        templateUrl:'partials/find.html',
+        controller: 'findController'
+    })
+    .when('/user_collections', {
+        templateUrl:'partials/all_user_collections.html',
+        controller: 'collectionController'
+    })
+    .when('/card/:id', {
+        templateUrl:'partials/flashcard.html',
+        controller: 'notecardController'
+    })
+    .when('/test/:id' , {
+        templateUrl:'partials/test.html',
+        controller: 'notecardController'
+    })
+    .when('/' , {
+        templateUrl:'partials',
+        controller: ''
+    })
+    .when('/' , {
+        templateUrl:'partials',
+        controller: ''
+    })
+    .when('/', {
+        templateUrl:'partials',
+        controller: ''
     })
     .otherwise({
         redirectTo: '/'
